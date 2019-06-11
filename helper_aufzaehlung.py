18ec2c8391315a7fa7a71f64dc21ebead78f80e2
mcenderdragon <goto.minecraft@googlemail.com>
2019-06-11 12:58:12 +0200

    added adjective word combination

--- helper_aufzaehlung.py
+++ helper_aufzaehlung.py
@@ -2,7 +2,7 @@
 def answer_as_array(token, only_childs = False):
 	words = []
 	if not only_childs:
-		words.append(token.text)
+		words.append(combine_adjectives_as_text(token))
 	for c in token.children:
 		if c.dep_ == "cj":
 			words.extend(answer_as_array(c, False))
@@ -13,3 +13,29 @@ def answer_as_array(token, only_childs = False):
 	
 	
 	return words
+
+
+
+def combine_adjectives(token):
+	words = dict()
+	words[token.i] = token.text
+	for c in token.children:
+		if token.pos_ == "NOUN" or token.pos_ == "PROPN":
+			if c.dep_ == "nk" and not c.pos_ == "DET":
+				words = {**words, **combine_adjectives(c)}
+			if c.dep_ == "pnc" and (c.pos_ == "NOUN" or c.pos_ == "PROPN"):
+				words = {**words, **combine_adjectives(c)}
+		elif token.pos_ == "ADJ":
+			if c.dep_ == "mo":
+				words = {**words, **combine_adjectives(c)}
+	return words
+
+def combine_adjectives_as_text(token, seperator = " "):
+	d = combine_adjectives(token)
+	print(d)
+	s = ""
+	for c in sorted(d.keys()): 
+		if len(s)>0 and s[-1] != seperator:
+			s += seperator
+		s += d[c]
+	return s
